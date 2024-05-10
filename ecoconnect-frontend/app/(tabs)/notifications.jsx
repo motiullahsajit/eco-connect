7570add907faa5b44b6ba332cf37040ab8437b14
Motiullah Sajit <sajitmotiullah@gmail.com>
2024-05-10 19:13:35 +0600

    inital layout added

---
+++ ecoconnect-frontend/app/(tabs)/notifications.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const notifications = () => {
+  return (
+    <View>
+      <Text>notifications</Text>
+    </View>
+  );
+};
+
+export default notifications;
+
+const styles = StyleSheet.create({});
--- ecoconnect-frontend/app/index.jsx
+++ ecoconnect-frontend/app/index.jsx
@@ -1,25 +1,63 @@
-import { Link } from "expo-router";
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const App = () => {
+import { images } from "../constants";
+import { CustomButton, Loader } from "../components";
+import { useGlobalContext } from "../context/GlobalProvider";
+
+const Welcome = () => {
+  const { loading, isLogged } = useGlobalContext();
+
+  if (!loading && isLogged) return <Redirect href="/home" />;
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world!!</Text>
-      <Link href="/profile" style={{ color: "black" }}>
-        Go To Profile
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <Loader isLoading={loading} />
+
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Eco Connect</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Eco Connect
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-  },
-});
-
-export default App;
+export default Welcome;
